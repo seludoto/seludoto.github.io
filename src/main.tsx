b2aac025818208f7ebc71b7ab25ba9f5ad994e67
SELU DOTO ISENGE <113712938+dolese@users.noreply.github.com>
2026-01-14 22:15:22 +0300

    Fix critical syntax error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import App from './App'
 import ErrorBoundary from './components/ErrorBoundary'
 import './styles/global.css'
 
-
-    <React.StrictMode>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </React.StrictMode>
-  )
-
-}
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+)
